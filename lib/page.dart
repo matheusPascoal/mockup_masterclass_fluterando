19a342173288e6dedca3ea55f2ae7f12cd8d3b3d
matheus leander <pascoalleander@gmail.com>
2022-07-23 15:50:16 -0300

    frist 1.0

---
+++ lib/page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FristPage extends StatefulWidget {
+  const FristPage({super.key});
+
+  @override
+  State<FristPage> createState() => _FristPageState();
+}
+
+class _FristPageState extends State<FristPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
